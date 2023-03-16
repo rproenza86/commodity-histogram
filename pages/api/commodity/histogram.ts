712ef011966da0a80615592b6b4cd3ed23fd1605
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2023-03-16 13:02:14 -0400

    chore: Code clean up

--- pages/api/commodity/histogram.ts
+++ pages/api/commodity/histogram.ts
@@ -1,8 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 import startHistogramProcessing, { HistogramColumns } from '../../../services/histogramProcessor';
 
-const handler = async (req, res) => {
-  const histogramResult = await startHistogramProcessing(HistogramColumns.Commodity);
-  res.status(200).json(histogramResult);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === 'GET') {
+    const histogramResult = await startHistogramProcessing(HistogramColumns.Commodity);
+    return res.status(200).json(histogramResult);
+  }
+    
+  return res.status(405).json({ message: 'Not found' });
 }
 
 export default handler;
--- pages/api/commodityType/histogram.ts
+++ pages/api/commodityType/histogram.ts
@@ -1,8 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 import startHistogramProcessing, { HistogramColumns } from '../../../services/histogramProcessor';
 
-const handler = async (req, res) => {
-  const histogramResult = await  startHistogramProcessing(HistogramColumns.CommodityType);
-  res.status(200).json(histogramResult);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    const histogramResult = await startHistogramProcessing(HistogramColumns.CommodityType);
+    return res.status(200).json(histogramResult);
+  
+  } 
+    
+  return res.status(405).json({ message: 'Not found' });
 }
 
 export default handler;
